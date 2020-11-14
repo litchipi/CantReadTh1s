9558d6acc1f1572b185dbf751af774e48e4c0f2c
John <john@sparta.pizza>
2020-11-14 17:05:46 +0100

    Fixes

--- cantreadth1s/__init__.py
+++ cantreadth1s/__init__.py
@@ -1,4 +1,4 @@
 from .cantreadth1s import CantReadThis, VERSION
-from .sec_tools import derive_key, process_pwd
+from .sec_tools import derive_key, test_header_password
 from .exceptions import BadPasswordException
 from .compression import CompressionWrapper
